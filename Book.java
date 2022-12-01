3ceae4591d979ed00432d436cb35091631f55ed0
swolinski <55709714+swolinski@users.noreply.github.com>
2022-12-01 15:17:50 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    public String title;
+    public int numPages;
+
+    public Book(String t, int n) {
+        this.title = t;
+        this.numPages = n;
+    }
+
+    public void printBook() {
+        System.out.println("The book " + title + " is " + numPages + " long.");
+    }
+}
---
+++ Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args){
+        Book myBook = new Book("Watership Down", 525);
+        myBook.printBook();
+    }
+}
